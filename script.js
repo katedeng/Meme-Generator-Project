04138d52d76fa55dfe2cd53dfc433cb192eb5c44
kate deng <katedeng2020@gmail.com>
2023-06-22 14:33:32 -0600

    test secussfully

--- script.js
+++ script.js
@@ -5,15 +5,13 @@
 function clearAll() {
   const meme = document.querySelector('.meme-content');
   const joke = document.querySelector('.joke-content');
-  const quote = document.querySelector('quote-content');
+  const quote = document.querySelector('.quote-content');
   const riddle = document.querySelector('.riddle-content');
 
-  meme.innerHTML = '';
-  joke.innerHTML = '';
-  quote.innerHTML = '';
+  meme.innerHTML ='';
+  joke.innerHTML ='';
+  quote.innerHTML ='';
   riddle.innerHTML = '';
-
-
 }
 
 /**
@@ -46,8 +44,9 @@ function showJoke() {
   const randomJokeText = getRandomData("jokes");
 
   const jokeContainer = document.querySelector('.joke-content');
-  const newJoke = document.createElement('.joke-content');
+  const newJoke = document.createElement('p');
   newJoke.textContent = randomJokeText;
+
 
   clearAll();
 
@@ -66,13 +65,15 @@ function showQuote() {
 
   const quoteContainer = document.querySelector('.quote-content');
   const newQuoteText = document.createElement('p');
-  const newQuoteAuthor = document.createElement('p ');
+  const newQuoteAuthor = document.createElement('p');
+
   newQuoteText.textContent = randomQuote.quote;
   newQuoteAuthor.textContent = '-' + randomQuote.author;
 
+
   clearAll();
 
-  // add the new joke to the document
+  // add the new quote to the document
   quoteContainer.appendChild(newQuoteText);
   quoteContainer.appendChild(newQuoteAuthor);
 
@@ -198,22 +199,22 @@ const jokes = [
 const quotes = [
   {
     quote:
-      "Programs must be written for people to read, and only incidentally for machines to execute.",
+        "Programs must be written for people to read, and only incidentally for machines to execute.",
     author: "Harold Abelson",
   },
   {
     quote:
-      "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.",
+        "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.",
     author: "Rick Cook",
   },
   {
     quote:
-      "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
+        "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
     author: "John Woods",
   },
   {
     quote:
-      "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
     author: "Martin Fowler",
   },
   {
@@ -222,37 +223,37 @@ const quotes = [
   },
   {
     quote:
-      "That's the thing about people who think they hate computers. What they really hate is lousy programmers.",
+        "That's the thing about people who think they hate computers. What they really hate is lousy programmers.",
     author: "Larry Niven",
   },
   {
     quote:
-      "You've baked a really lovely cake, but then you've used dog shit for frosting.",
+        "You've baked a really lovely cake, but then you've used dog shit for frosting.",
     author: "Steve Jobs",
   },
   {
     quote:
-      "A language that doesn't affect the way you think about programming is not worth knowing.",
+        "A language that doesn't affect the way you think about programming is not worth knowing.",
     author: "Alan J. Perlis",
   },
   {
     quote:
-      "The most disastrous thing that you can ever learn is your first programming language.",
+        "The most disastrous thing that you can ever learn is your first programming language.",
     author: "Alan Kay",
   },
   {
     quote:
-      "The computer programmer is a creator of universes for which he alone is the lawgiver. No playwright, no stage director, no emperor, however powerful, has ever exercised such absolute authority to arrange a stage or field of battle and to command such unswervingly dutiful actors or troops.",
+        "The computer programmer is a creator of universes for which he alone is the lawgiver. No playwright, no stage director, no emperor, however powerful, has ever exercised such absolute authority to arrange a stage or field of battle and to command such unswervingly dutiful actors or troops.",
     author: "Joseph Weizenbaum",
   },
   {
     quote:
-      "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?",
+        "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?",
     author: "Brian Kernighan",
   },
   {
     quote:
-      "No matter which field of work you want to go in, it is of great importance to learn at least one programming language.",
+        "No matter which field of work you want to go in, it is of great importance to learn at least one programming language.",
     author: "Ram Ray",
   },
 ];
@@ -261,32 +262,32 @@ const quotes = [
 const riddles = [
   {
     question:
-      "I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?",
+        "I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?",
     answer: "An echo",
   },
   {
     question:
-      "You measure my life in hours and I serve you by expiring. I’m quick when I’m thin and slow when I’m fat. The wind is my enemy. ",
+        "You measure my life in hours and I serve you by expiring. I’m quick when I’m thin and slow when I’m fat. The wind is my enemy. ",
     answer: "A Candle",
   },
   {
     question:
-      "I have cities, but no houses. I have mountains, but no trees. I have water, but no fish. What am I? ",
+        "I have cities, but no houses. I have mountains, but no trees. I have water, but no fish. What am I? ",
     answer: "A Map",
   },
   {
     question:
-      "What is seen in the middle of March and April that can’t be seen at the beginning or end of either month?",
+        "What is seen in the middle of March and April that can’t be seen at the beginning or end of either month?",
     answer: 'The letter "R"',
   },
   {
     question:
-      "You see a boat filled with people. It has not sunk, but when you look again you don’t see a single person on the boat. Why?",
+        "You see a boat filled with people. It has not sunk, but when you look again you don’t see a single person on the boat. Why?",
     answer: "All the people were married",
   },
   {
     question:
-      "What word in the English language does the following: the first two letters signify a male, the first three letters signify a female, the first four letters signify a great, while the entire world signifies a great woman. What is the word?",
+        "What word in the English language does the following: the first two letters signify a male, the first three letters signify a female, the first four letters signify a great, while the entire world signifies a great woman. What is the word?",
     answer: "Heroine",
   },
 ];
